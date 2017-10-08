e393c90bfa72598fd139cdbe3b54409fc57ccf2a
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-10-08 15:49:30 -0400

    Exercise 45 (First Commit)

---
+++ Exercise45/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                //Serena Tindle-- Exercise45 (Area and Perimeter of a rectangle)
+                //TODO
+                //Fix validation method error
+
+                bool ContinueProgram = true;
+                while (ContinueProgram == true)
+                {
+
+                    Square RectangleLength = new Square(); //create an object
+
+                    Console.WriteLine("Enter a side length:"); //prompt
+                    RectangleLength.SideLength = ReadValidDouble(); //feed object
+                    Console.WriteLine("The square has a side length of" + "Its area is" + RectangleLength.GetArea() + "and its perimeter is" + RectangleLength.GetPerimeter());
+
+                    string UserOption; //Continue program or exit?
+                    Console.WriteLine("Would you like to continue? (y/n)");
+                    UserOption = Console.ReadLine();
+
+                    if (UserOption == "N" || UserOption == "No" || UserOption == "no" || UserOption == "n")
+                    {
+                        ContinueProgram = false;
+                        Console.WriteLine("Goodbye!");
+                    }
+
+                    Console.ReadKey();
+                }
+            }
+
+            public static double ReadValidDouble() //method for validation 
+            {
+                double.Parse(Console.ReadLine());
+                double input = 0;
+
+                while (!double.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine("Please enter valid data");
+                }
+                return input;
+
+            }
+        }
+    }
+}
---
+++ Exercise45/Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise45
+{
+    class Square
+    {
+
+        private double lengthofside;
+
+        //property
+        public double SideLength
+
+        {
+            set { lengthofside = value; }
+            get { return lengthofside; }
+        }
+
+        //methods
+        public double GetArea()
+        {
+            double area = lengthofside * lengthofside;
+            return area;
+        }
+        
+
+        public double GetPerimeter()
+        {
+            double perimeter;
+            perimeter = 2 * (lengthofside + lengthofside);
+            return perimeter;
+            }
+        }
+    }
+
